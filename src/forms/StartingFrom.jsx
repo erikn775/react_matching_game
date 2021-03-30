89bf6607f53537b091a1ac8e5430750ffaa96859
erikn775 <erikn775@gmail.com>
2021-03-30 17:36:26 -0600

    started opening form

---
+++ src/forms/StartingFrom.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import {Form, Button} from 'react-bootstrap';
+
+export default function StartingForm() {
+    return(
+        <Form onSubmit={formSubmit}>
+            
+        </Form>
+    )
+}
