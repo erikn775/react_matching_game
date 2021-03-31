261e42eabc0ba559c701595b8798e461a7b783d2
erikn775 <erikn775@gmail.com>
2021-03-30 19:15:39 -0600

    added card component

---
+++ src/components/Card.jsx
@@ -0,0 +1,19 @@
+import React, {useState} from 'react';
+import { useSpring, animated as a } from 'react-spring';
+import '../stylesheets/card.css'
+
+export default function Card() {
+    const [flipped, set] = useState(false);
+    const { transform, opacity } = useSpring({
+        opacity: flipped ? 1 : 0,
+        transform: `perspective(600px) rotateX(${flipped ? 180 : 0}deg)`,
+        config: { mass: 5, tension: 500, friction: 80 }
+      })
+
+    return(
+        <div className="card" onClick={() => set(state => !state)}>
+            <a.div className="c back" style={{opacity: opacity.to(o => 1 - o), transform}} />
+            <a.div className="c front" style={{ opacity, transform: transform.to(t => `${t} rotateX(180deg)`) }} />
+        </div>
+    )
+}
--- src/forms/StartingFrom.jsx
+++ src/forms/StartingFrom.jsx
@@ -1,10 +1,42 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import {Form, Button} from 'react-bootstrap';
 
+
 export default function StartingForm() {
+    const [state, setState] = useState({})
+
+    function formSubmit() {
+        useEffect(() => {
+            fetch()
+            .then(response => response.json())
+            .then(
+                responseData => setState(responseData)
+            )
+        })
+    }
+
+
     return(
-        <Form onSubmit={formSubmit}>
-            
-        </Form>
+        <div className="start-form">
+            <h3>Matching Game</h3>
+            <Form onSubmit={formSubmit}>
+                <Form.Group controlId="start-name">
+                    <Form.Label>Name</Form.Label>
+                    <Form.Control onChange={this.onFormChange} name="name" type="text" />
+                </Form.Group>
+                <Form.Group controlId="start-card-select">
+                    <Form.Label>Card Deck</Form.Label>
+                    <Form.Control as="select" onChange={this.onFormChange} name="card">
+                        <option value="">Please Select...</option>
+                        <option value="1">1</option>
+                        <option value="2">2</option>
+                        <option value="3">3</option>
+                        <option value="4">4</option>
+                        <option value="5">5</option>
+                    </Form.Control>
+                </Form.Group>
+            </Form>
+        </div>
+        
     )
 }
