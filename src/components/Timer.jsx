bde129fa1f7e6aaaa919014b0270bf3d513cc57f
erikn775 <erikn775@gmail.com>
2021-04-15 22:37:41 -0600

    timer now counts properly

--- src/components/Timer.jsx
+++ src/components/Timer.jsx
@@ -1,18 +1,32 @@
 import React, { useEffect, useState } from 'react';
 
 export default function Timer(){
-    const [second, setSecond] = useState(0);
-    const [minute, setMinute] = useState(0);
+    const [seconds, setSeconds] = useState(0);
+    const [minutes, setMinutes] = useState(0);
+    const [isActive, setIsActive] = useState(false)
 
     useEffect(() => {
-        let interval = setInterval(function(){
-            setSecond(second + 1)
-        }, 1000)
-    });
+        let interval = null;
+        if(isActive){
+            interval = setInterval(() => {
+                setSeconds(seconds => seconds + 1)
+                if(seconds > 58){
+                    setMinutes(minutes => minutes + 1)
+                    setSeconds(0)
+                }
+            }, 1000)
+        }
+        else if(!isActive && seconds !== 0){
+            clearInterval(interval)
+        }
+        return () => clearInterval(interval);
+         
+    }, [isActive, seconds]);
 
     return(
         <div>
-            <h3>Time: {minute}min {second}sec</h3>
+            <h3>Time: {minutes}min {seconds}sec</h3>
+            <button onClick={() => setIsActive(!isActive)}>Start!</button>
         </div>
     )
 }
